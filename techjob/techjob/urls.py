3ef907ca1f00881839f390d0961d35e99ec84044
GuilhermeRangelSilva <162377684+GuilhermeRangelSilva@users.noreply.github.com>
2024-09-28 13:02:26 -0300

    Update urls.py

--- techjob/techjob/urls.py
+++ techjob/techjob/urls.py
@@ -2,7 +2,7 @@ from app import views
 from django.urls import path
 
 urlpatterns = [
-    path('a', views.login, name='login'),
+    path('', views.login, name='login'),
     path('cadastro/',views.cadastro, name = 'cadastro'),
-    path('',views.home,name = 'home'),
+    path('home/',views.home,name = 'home'),
 ]
