c7a6e9f809d5cb68c3b1714a592f1539d5675a3c
GuilhermeRangelSilva <162377684+GuilhermeRangelSilva@users.noreply.github.com>
2024-09-28 17:20:59 -0300

    path

--- techjob/app/models.py
+++ techjob/app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    TIPO_USUARIO_CHOICES = [
+        ('freelancer', 'Freelancer'),
+        ('empresa', 'Empresa'),
+    ]
+    
+    usuario = models.CharField(max_length=150, unique=True)  # Nome de usuário
+    tipo_usuario = models.CharField(max_length=10, choices=TIPO_USUARIO_CHOICES)  # Tipo de usuário
+
+    def __str__(self):
+        return self.usuario
+
+class Freelancer(models.Model):
+    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, related_name='freelancer')
+    nome = models.CharField(max_length=150)  # Nome do freelancer
+    cpf = models.CharField(max_length=11, unique=True)  # CPF
+    email = models.EmailField(unique=True)  # E-mail
+    github = models.URLField(blank=True, null=True)  # GitHub
+
+    def __str__(self):
+        return self.nome
+
+class Empresa(models.Model):
+    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, related_name='empresa')
+    nome_empresa = models.CharField(max_length=150)  # Nome da empresa
+    cnpj = models.CharField(max_length=14, unique=True)  # CNPJ
+    email = models.EmailField(unique=True)  # E-mail da empresa
+
+    def __str__(self):
+        return self.nome_empresa
--- techjob/techjob/urls.py
+++ techjob/techjob/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('cadastro/',views.cadastro, name = 'cadastro'),
     path('home/',views.home,name = 'home'),
-    path('usuario_freelancer',views.freelancer, name = 'freelancer'),
-    path('usuario_empresa',views.empresa, name = 'empresa'),
+    path('usuario_freelancer/',views.freelancer, name = 'freelancer'),
+    path('usuario_empresa/',views.empresa, name = 'empresa'),
 ]
